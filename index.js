89de9c336e1f7561279f997534147fe8c2282cdd
Sahil Verma <107564386+shl082002@users.noreply.github.com>
2023-11-09 16:15:44 +0530

    button animation added

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// var acc = document.getElementsByClassName("accordion-container");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function () {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     }
-//   });
-// }
-// function readHomePage() {}
 
 document.addEventListener("DOMContentLoaded", function () {
   const accordionItems = document.querySelectorAll(".acc-item");
@@ -34,7 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// Simulate a click event on the "DESCRIPTION" button when the page loads
 document.addEventListener("DOMContentLoaded", function () {
   const descriptionButton = document.querySelector(
     'button[data-option="description"]'
@@ -43,31 +27,23 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Initially, show the content of the "DESCRIPTION" button
   selectOption("description");
 });
 
 function selectOption(option) {
-  // Get all option containers
   const optionContainers = document.querySelectorAll(".option-content");
 
-  // Hide all option containers
   optionContainers.forEach((container) => {
     container.style.display = "none";
   });
-
-  // Show the content of the selected button
   const selectedOption = document.getElementById(option);
   selectedOption.style.display = "block";
 
-  // Reset button styles
   const buttons = document.querySelectorAll(".options");
   buttons.forEach((button) => {
     button.style.backgroundColor = "";
     button.style.color = "";
   });
-
-  // Set the style for the selected button
   const selectedButton = document.querySelector(
     `button[data-option="${option}"]`
   );
@@ -75,16 +51,25 @@ function selectOption(option) {
   selectedButton.style.color = "white";
 }
 
-// window.onscroll = function () {
-//   const mainNav = document.getElementById('roto');
-//   // const upbtn = document.getElementById("upBtn");
-//   if (mainNav) {
-//       if (window.pageYOffset > 1) {
-//           mainNav.classList.add("active");
-//           // upbtn.classList.add("active");
-//       } else {
-//           mainNav.classList.remove("active");
-//           // upbtn.classList.remove("active");
-//       }
-//   }
-// }
+// document.addEventListener("DOMContentLoaded", function () {
+//   const firstbutton = document.querySelector(
+//     'button[data-option="description"]'
+//   );
+//   firstbutton.click();
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   selectOptionbtn("description");
+// });
+
+function selectOptionbtn(option) {
+  const buttons = document.querySelectorAll(".quantity-subbox");
+  buttons.forEach((button) => {
+    button.style.backgroundColor = "";
+    button.style.color = "";
+  });
+  const selectedButton = document.querySelector(
+    `button[data-option="${option}"]`
+  );
+  selectedButton.style.backgroundColor = "#9eef80";
+}
